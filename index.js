3a847eb39a50d5410c4116c832bf09f84934b6d2
lianghansong <1312128240@qq.com>
2020-02-09 06:40:39 -0800

    iponeX测试

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import routes from './lib/AppRoutes'
-
+//react-native run-ios -- simulator='iPhone 11'
 AppRegistry.registerComponent(appName, () =>routes);
